99fa7dbf32ccccdfb759a790d6813ef72c331e0e
Irving Juárez <irvingjuarez274@gmail.com>
2022-12-19 19:22:30 -0600

    tailwind config added

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js, jsx, ts, tsx}"
+  ],
+  purge: [
+    './src/**/*.{vue,js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
