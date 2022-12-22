37d0535f08441aae6af5908cfe9add6cf4afebb6
Irving Juárez <irvingjuarez274@gmail.com>
2022-12-22 11:52:02 -0600

    Input component broaded and the inputs in the new game page are listened

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,8 +1,11 @@
 type InputProps = {
 	children: string;
+	ref?: React.MutableRefObject<HTMLInputElement | null>;
+	name?: string;
+	handleChange?: (evt: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export const Input: React.FC<InputProps> = ({ children }) => {
+export const Input: React.FC<InputProps> = ({ children, ref, name, handleChange }) => {
 	const inputID = children.split(" ").join("-")
 
 	return (
@@ -12,6 +15,9 @@ export const Input: React.FC<InputProps> = ({ children }) => {
 			</label>
 
 			<input
+				onChange={handleChange}
+				name={name}
+				ref={ref}
 				type="text"
 				id={inputID}
 				className="w-full rounded-md p-1 bg-background border-2 border-white outline-none text-white"
--- src/pages/NewGame/index.tsx
+++ src/pages/NewGame/index.tsx
@@ -2,16 +2,43 @@ import { ButtonsContainer } from "@app/containers/ButtonsContainer"
 import { Button } from "@app/components/Button"
 import { Form } from "@app/containers/Form"
 import { Input } from "@app/components/Input"
+import { useRef, useState } from "react"
 
 export const NewGame = () => {
+	const questionInputRef = useRef<HTMLInputElement | null>(null)
+	const answerInputRef = useRef<HTMLInputElement | null>(null)
+
+	const [inputsValues, setInputValues] = useState<{[key:string]: string}>({
+		questionInput: "",
+		answerInput: ""
+	})
+
+	const updateInputs = (evt: React.ChangeEvent<HTMLInputElement>) => {
+		const name = evt.target.name as string
+		setInputValues(prev => ({
+			...prev,
+			[name]: evt.target.value
+		}))
+	}
+
+	console.log(inputsValues)
+
 	return (
 		<section>
 			<Form>
-				<Input>
+				<Input
+					ref={questionInputRef}
+					name="questionInput"
+					handleChange={updateInputs}
+				>
 					Type your question:
 				</Input>
 
-				<Input>
+				<Input
+					ref={answerInputRef}
+					name="answerInput"
+					handleChange={updateInputs}
+				>
 					Add answer's option:
 				</Input>
 			</Form>
