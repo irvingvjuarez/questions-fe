3726e1a9be27713b2283de7c1f3e2efb36658dca
Irving Juárez <irvingjuarez274@gmail.com>
2022-12-26 11:45:39 -0600

    If the question id doesnt exist in the Options page, it returns back to the index

--- src/pages/Options/index.tsx
+++ src/pages/Options/index.tsx
@@ -1,4 +1,22 @@
+import { questionsContext } from "@app/contexts/questions.context"
+import { Questions } from "@app/types"
+import { useContext, useEffect } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+
 export const Options = () => {
+	const navigate = useNavigate()
+
+	const {questionId} = useParams()
+	const {questions} = useContext(questionsContext) as Questions
+	const currentQuestion = questions.find(question => question.id == questionId)
+
+	useEffect(() => {
+		if (!currentQuestion) {
+			// TODO: send to a 404 page
+			navigate("/")
+		}
+	}, [])
+
 	return (
 		<section>
 			<h2>Choose the correct option for the question:</h2>
