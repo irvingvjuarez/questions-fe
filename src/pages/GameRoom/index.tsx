d14ba42eecebf080ab8f46fd9039433c29952fec
Irving Juárez <irvingjuarez274@gmail.com>
2022-12-26 19:40:54 -0600

    Validation in the GameRoom page done

--- src/pages/GameRoom/index.tsx
+++ src/pages/GameRoom/index.tsx
@@ -1,4 +1,20 @@
+import { questionsContext } from "@app/contexts/questions.context"
+import { Questions } from "@app/types"
+import { useContext, useEffect } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+
 export const GameRoom = () => {
+	const navigate = useNavigate()
+
+	const { gameCode: gameCodeParam } = useParams()
+	const { gameCode: createdGameCode } = useContext(questionsContext) as Questions;
+
+	useEffect(() => {
+		if(Number(gameCodeParam) !== createdGameCode) {
+			navigate("/")
+		}
+	}, [])
+
 	return (
 		<section>
 			<h2>I am the GameRoom page</h2>
