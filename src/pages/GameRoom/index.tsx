5ecd9221032bbf98d022b6a0c8a45a9ecc11c914
Irving Juárez <irvingjuarez274@gmail.com>
2022-12-27 18:47:43 -0600

    GameRoom styles added

--- src/pages/GameRoom/index.tsx
+++ src/pages/GameRoom/index.tsx
@@ -1,10 +1,14 @@
+import { Button } from "@app/components/Button"
+import { ButtonsContainer } from "@app/containers/ButtonsContainer"
 import { questionsContext } from "@app/contexts/questions.context"
 import { Q_TYPES } from "@app/globals"
-import { Action, Questions } from "@app/types"
-import { useContext, useEffect } from "react"
+import { Action, Questions, User } from "@app/types"
+import { useContext, useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
 
 export const GameRoom = () => {
+	const [currentUsers, setCurrentUsers] = useState<User[]>([])
+
 	const navigate = useNavigate()
 	const { gameCode: contextGameCode } = useContext(questionsContext) as Questions
 	const { gameCode: paramGameCode } = useParams()
@@ -24,6 +28,28 @@ export const GameRoom = () => {
 			<span className="highlighted">
 				{paramGameCode}
 			</span>
+
+			<article className="border-2 border-white w-[90%] mx-auto h-[60vh] my-3 flex justify-center items-center p-3">
+				{currentUsers.length < 1 ? (
+					<span>
+						This game room is currently empty.
+					</span>
+				) : (
+					<>
+						{currentUsers.map(user => (
+							<span>
+								{user.nickname}
+							</span>
+						))}
+					</>
+				)}
+			</article>
+
+			<ButtonsContainer>
+				<Button variant="active">
+					Start Game!
+				</Button>
+			</ButtonsContainer>
 		</section>
 	)
 }
