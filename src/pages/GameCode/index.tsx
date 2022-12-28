c8d4000e93ac89c7b387f8b76ff74500af9255d6
Irving Juárez <irvingjuarez274@gmail.com>
2022-12-27 19:34:13 -0600

    Post fetch in the GameCode page already done

--- src/pages/GameCode/index.tsx
+++ src/pages/GameCode/index.tsx
@@ -1,23 +1,61 @@
+import { useRef } from "react";
 import { Form } from "@app/containers/Form"
 import { Input } from "@app/components/Input"
 import { ButtonsContainer } from "@app/containers/ButtonsContainer"
 import { Button } from "@app/components/Button"
+import { API_ROOT } from "@app/globals"
 
 export const GameCode = () => {
+	const nicknameRef = useRef<HTMLInputElement | null>(null)
+	const gameCodeRef = useRef<HTMLInputElement | null>(null)
+
+	const enterGame = () => {
+		const nickname = nicknameRef.current ? nicknameRef.current.value : ""
+		const gameCode = gameCodeRef.current ? gameCodeRef.current.value : null
+
+		const fetchConfig = {
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			method: "POST",
+			body: JSON.stringify({ nickname })
+		}
+
+		fetch(API_ROOT + `/user/${gameCode}/join`, fetchConfig)
+			.then(res => {
+				if (res.status === 404) {
+					// Handling the error of game code not found
+					console.log("Game not found")
+				} else if (res.ok) {
+					return res.json()
+				}
+			})
+			.then(data => {
+				console.log({ data })
+			})
+			.catch(err => {
+				console.log({ err })
+			})
+	}
+
 	return (
 		<section>
 			<Form>
-				<Input>
+				<Input ref={gameCodeRef}>
 					Enter the game code:
 				</Input>
 
-				<Input>
+				<Input ref={nicknameRef}>
 					Enter your nickname:
 				</Input>
 			</Form>
 
 			<ButtonsContainer>
-				<Button variant="active">
+				<Button
+					variant="active"
+					handleClick={enterGame}
+				>
 					Enter to the Game!
 				</Button>
 			</ButtonsContainer>
