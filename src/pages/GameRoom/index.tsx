c29b0eea6a35dd98b6a2fde72fea31aa43023b22
Irving Juárez <irvingjuarez274@gmail.com>
2022-12-28 19:37:35 -0600

    Updating the game room done

--- src/pages/GameRoom/index.tsx
+++ src/pages/GameRoom/index.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@app/components/Button"
 import { ButtonsContainer } from "@app/containers/ButtonsContainer"
 import { questionsContext } from "@app/contexts/questions.context"
-import { Q_TYPES } from "@app/globals"
+import { API_ROOT, Q_TYPES } from "@app/globals"
 import { Action, Questions, User } from "@app/types"
 import { useContext, useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
@@ -11,9 +11,25 @@ export const GameRoom = () => {
 	const { gameCode: contextGameCode, gameUsers = [], questions, isUser } = useContext(questionsContext) as Questions
 	const { gameCode: paramGameCode } = useParams();
 
+	const [currentUsers, setCurrentUsers] = useState<User[]>(gameUsers)
+
+	const fetchUsers = () => {
+		fetch(API_ROOT + `/game/${contextGameCode}/users`)
+			.then(res => res.json())
+			.then(data => {
+				const newUsers = data.users.map((user: User) => ({
+					nickname: user.nickname
+				}))
+
+				setCurrentUsers(newUsers)
+			})
+	}
+
 	useEffect(() => {
 		if (Number(paramGameCode) !== contextGameCode) {
 			navigate("/")
+		} else {
+			const getUsersInterval = setInterval(fetchUsers, 1000)
 		}
 	}, [])
 
@@ -36,13 +52,13 @@ export const GameRoom = () => {
 			)}
 
 			<article className="border-2 border-white w-[90%] mx-auto h-[60vh] my-3 flex justify-center items-center p-3">
-				{gameUsers.length < 1 ? (
+				{currentUsers.length < 1 ? (
 					<span>
 						This game room is currently empty.
 					</span>
 				) : (
 					<div className="w-full flex h-[inherit] py-2 space-x-1">
-						{gameUsers.map(user => (
+						{currentUsers.map(user => (
 							<span
 								key={user.nickname}
 								className="bg-white text-background-dark font-semibold p-1 rounded-xl h-fit"
