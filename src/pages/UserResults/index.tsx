5e548fd49f799e4683fba6295f5eb8153bad92c1
Irving Juárez <irvingjuarez274@gmail.com>
2023-01-11 17:07:34 -0600

    UserResults page listening to changes in the game

--- src/pages/UserResults/index.tsx
+++ src/pages/UserResults/index.tsx
@@ -5,19 +5,46 @@ import { useContext, useEffect } from "react"
 import right from "@app/assets/right.png"
 import wrong from "@app/assets/wrong.png"
 import { useErrorValidation } from "@app/hooks/useErrorValidation"
-import { Q_TYPES } from "@app/globals"
+import { API_ROOT, Q_TYPES } from "@app/globals"
+import { useNavigate } from "react-router-dom"
 
 export const UserResults = () => {
 	const validation = useErrorValidation()
-	const { answeredQuestion, user: {nickname}, questionsDispatch } = useContext(questionsContext) as Questions
+	const navigate = useNavigate()
+	let gameRestartedInterval: number
+
+	const { answeredQuestion, user: {nickname}, questionsDispatch, gameCode } = useContext(questionsContext) as Questions
 	const dispatch = questionsDispatch as React.Dispatch<Action>
 
 	const isUserRight = answeredQuestion?.isUserCorrect
 	const userData = answeredQuestion?.answeredBy.find(user => user.userNickname == nickname)
 
+	const isGameRestarted = () => {
+		fetch(API_ROOT + `/user/${gameCode}/current/question/status`)
+			.then(res => {
+				if (!res.ok) throw new Error()
+				return res.json()
+			})
+			.then(data => {
+				let navigateEndpoint
+
+				if (data.isGameOver) navigateEndpoint = "/game/over"
+				if (data.status.counterActive) navigateEndpoint = `/game/${gameCode}/current/question`
+
+				if (navigateEndpoint) {
+					dispatch({ type: Q_TYPES.clearAnsweredQuestion })
+					clearInterval(gameRestartedInterval)
+					navigate(navigateEndpoint)
+				}
+			})
+			.catch(() => navigate("/"))
+	}
+
 	useEffect(() => {
 		validation()
-		// dispatch({ type: Q_TYPES.clearAnsweredQuestion })
+		gameRestartedInterval = setInterval(isGameRestarted, 1000)
+
+		return () => clearInterval(gameRestartedInterval)
 	}, [])
 
 	return(
