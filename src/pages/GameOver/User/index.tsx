475371205c7bd3c923d073b9de75e94733ec74aa
Irving Juárez <irvingjuarez274@gmail.com>
2023-01-13 18:54:11 -0600

    Maquetation of User GameOver page done

--- src/pages/GameOver/User/index.tsx
+++ src/pages/GameOver/User/index.tsx
@@ -1,6 +1,32 @@
-import { Fragment } from "react"
+import { questionsContext } from "@app/contexts/questions.context"
+import { Questions } from "@app/types"
+import { Fragment, useContext } from "react"
+
+import winner from "@app/assets/trophy.png"
+import loser from "@app/assets/disappointed.png"
 
 export const GameOverUser = () => {
+	const { score, user: {nickname} } = useContext(questionsContext) as Questions
+	const scoreIndex = score.findIndex(user => user.userNickname == nickname)
+
+	let position
+
+	switch (scoreIndex) {
+		case 0:
+			position = "first"
+			break;
+		case 1:
+			position = "second"
+			break;
+		case 2:
+			position = "third"
+			break;
+	}
+
+	const message = scoreIndex <= 2
+		? `You got ${position} place, Congrats!`
+		: "You were not that fast this time. Good luck the next time"
+
 	return (
 		<Fragment>
 			<h2 className="subtitle">
@@ -8,7 +34,11 @@ export const GameOverUser = () => {
 			</h2>
 
 			<div>
-				Results
+				<img src={scoreIndex <= 2 ? winner : loser} alt="" />
+
+				<h3>
+					{message}
+				</h3>
 			</div>
 
 			<p>
