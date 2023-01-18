0fac04266ecb55d4f0e8ea7a89cb8faef371ff05
Irving Juárez <irvingjuarez274@gmail.com>
2023-01-18 12:30:57 -0600

    User gameover page done

--- src/pages/GameOver/User/index.tsx
+++ src/pages/GameOver/User/index.tsx
@@ -8,51 +8,52 @@ import loser from "@app/assets/disappointed.png"
 export const GameOverUser = () => {
 	const { user, score } = useContext(questionsContext) as Questions
 	const nickname = user.nickname
-	const scoreIndex = score?.findIndex(user => user.userNickname == nickname)
-	let position: string, message
+	const scoreIndex = score.findIndex(item => item.nickname == nickname)
 
-	useEffect(() => {
-		switch (scoreIndex) {
-			case 0:
-				position = "first"
-				break;
-			case 1:
-				position = "second"
-				break;
-			case 2:
-				position = "third"
-				break;
-		}
+	let position = "", message
 
-		message = scoreIndex <= 2
-			? `You got ${position} place, Congrats!`
-			: "You were not that fast this time. Good luck the next time"
-	}, [])
-
-
-	if (score.length) {
-		return (
-			<Fragment>
-				<h2 className="subtitle">
-					Questionnaire finished
-				</h2>
-
-				<div>
-					<img src={scoreIndex <= 2 ? winner : loser} alt="" />
-
-					<h3>
-						{message}
-					</h3>
-				</div>
-
-				<p>
-					Total points: <span className="highlighted">1928</span>
-				</p>
-			</Fragment>
-		)
-	} else {
-		return (
-			<h2>Loading...</h2>
-		)
+	switch (scoreIndex) {
+		case 0:
+			position = "first"
+			break;
+		case 1:
+			position = "second"
+			break;
+		case 2:
+			position = "third"
+			break;
 	}
+
+	message = scoreIndex <= 2
+		? `You got ${position} place, Congrats!`
+		: "You were not that fast this time. Good luck the next time"
+
+
+	return (
+		<Fragment>
+			<h2 className="subtitle">
+				Questionnaire finished
+			</h2>
+
+			<div className="my-4">
+				<img
+					src={scoreIndex <= 2 ? winner : loser}
+					alt=""
+					className="mx-auto w-[200px]"
+				/>
+
+				<h3 className="subtitle">
+					{message}
+				</h3>
+			</div>
+
+			<p>
+				Total points:
+
+				<span className="highlighted">
+					{score[scoreIndex].score}
+				</span>
+			</p>
+		</Fragment>
+	)
 }
