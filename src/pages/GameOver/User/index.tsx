4ea352b8788ee94e84a332de8fb583c54dd90a22
Irving Juárez <irvingjuarez274@gmail.com>
2023-01-18 11:51:37 -0600

    Unusing User and Admin components, but modification in the data fetching are gonna solve the problem

--- src/pages/GameOver/User/index.tsx
+++ src/pages/GameOver/User/index.tsx
@@ -1,54 +1,58 @@
 import { questionsContext } from "@app/contexts/questions.context"
 import { Questions, Score, User } from "@app/types"
-import { Fragment, useContext } from "react"
+import { Fragment, useContext, useEffect } from "react"
 
 import winner from "@app/assets/trophy.png"
 import loser from "@app/assets/disappointed.png"
 
-type GameOverUserProps = {
-	user: User;
-	score: Questions["score"]
-}
-
-export const GameOverUser: React.FC<GameOverUserProps> = ({ user, score }) => {
+export const GameOverUser = () => {
+	const { user, score } = useContext(questionsContext) as Questions
 	const nickname = user.nickname
 	const scoreIndex = score?.findIndex(user => user.userNickname == nickname)
+	let position: string, message
 
-	let position
+	useEffect(() => {
+		switch (scoreIndex) {
+			case 0:
+				position = "first"
+				break;
+			case 1:
+				position = "second"
+				break;
+			case 2:
+				position = "third"
+				break;
+		}
 
-	switch (scoreIndex) {
-		case 0:
-			position = "first"
-			break;
-		case 1:
-			position = "second"
-			break;
-		case 2:
-			position = "third"
-			break;
+		message = scoreIndex <= 2
+			? `You got ${position} place, Congrats!`
+			: "You were not that fast this time. Good luck the next time"
+	}, [])
+
+
+	if (score.length) {
+		return (
+			<Fragment>
+				<h2 className="subtitle">
+					Questionnaire finished
+				</h2>
+
+				<div>
+					<img src={scoreIndex <= 2 ? winner : loser} alt="" />
+
+					<h3>
+						{message}
+					</h3>
+				</div>
+
+				<p>
+					Total points: <span className="highlighted">1928</span>
+				</p>
+			</Fragment>
+		)
+	} else {
+		return (
+			<h2>Loading...</h2>
+		)
 	}
-
-	const message = scoreIndex <= 2
-		? `You got ${position} place, Congrats!`
-		: "You were not that fast this time. Good luck the next time"
-
-	return (
-		<Fragment>
-			<h2 className="subtitle">
-				Questionnaire finished
-			</h2>
-
-			<div>
-				<img src={scoreIndex <= 2 ? winner : loser} alt="" />
-
-				<h3>
-					{message}
-				</h3>
-			</div>
-
-			<p>
-				Total points: <span className="highlighted">1928</span>
-			</p>
-		</Fragment>
-	)
 }
--- src/pages/GameOver/index.tsx
+++ src/pages/GameOver/index.tsx
@@ -1,14 +1,40 @@
 import { questionsContext } from "@app/contexts/questions.context"
 import { useErrorValidation } from "@app/hooks/useErrorValidation"
 import { Questions } from "@app/types"
-import { useContext, useEffect } from "react"
+import { Fragment, useContext, useEffect } from "react"
 import { GameOverAdmin } from "./Admin"
 import { GameOverUser } from "./User"
 
+import winner from "@app/assets/trophy.png"
+import loser from "@app/assets/disappointed.png"
+import { WinnerTable } from "@app/containers/WinnerTable"
+import { ButtonsContainer } from "@app/containers/ButtonsContainer"
+import { Button } from "@app/components/Button"
+
 export const GameOver = () => {
 	const validation = useErrorValidation()
+	let position = ""
+
 	const { user, score } = useContext(questionsContext) as Questions
-	const userProps = { user, score }
+	const scoreIndex = score?.findIndex(item => item.userNickname == user.nickname)
+
+	// console.log({ score, scoreIndex })
+
+	switch (scoreIndex) {
+		case 0:
+			position = "first"
+			break;
+		case 1:
+			position = "second"
+			break;
+		case 2:
+			position = "third"
+			break;
+	}
+
+	const message = scoreIndex <= 2
+		? `You got ${position} place, Congrats!`
+		: "You were not that fast this time. Good luck the next time"
 
 	useEffect(() => {
 		validation()
@@ -16,7 +42,35 @@ export const GameOver = () => {
 
 	return (
 		<section className="page-container h-[70vh] flex flex-col justify-between">
-			{user.isUser ? <GameOverUser {...userProps} /> : <GameOverAdmin />}
+			{user.isUser ? ( // User scenario
+				<Fragment>
+					<h2 className="subtitle">
+						Questionnaire finished
+					</h2>
+
+					<div>
+						<img src={scoreIndex <= 2 ? winner : loser} alt="" />
+
+						<h3>
+							{message}
+						</h3>
+					</div>
+
+					<p>
+						Total points: <span className="highlighted">1928</span>
+					</p>
+				</Fragment>
+			) : ( // Admin scenario
+				<Fragment>
+					<WinnerTable />
+
+					<ButtonsContainer>
+						<Button variant="active">
+							Finish Game!
+						</Button>
+					</ButtonsContainer>
+				</Fragment>
+			)}
 		</section>
 	)
 }
