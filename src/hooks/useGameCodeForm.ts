0013eefb1f454a7dcd69cfc7c637d3ccbc0034fa
Irving Juárez <irvingjuarez274@gmail.com>
2023-01-24 21:27:03 -0600

    new custom hook added

---
+++ src/hooks/useGameCodeForm.ts
@@ -0,0 +1,52 @@
+import { questionsContext } from "@app/contexts/questions.context";
+import { API_ROOT, Q_TYPES } from "@app/globals";
+import { getPostConfig } from "@app/services/getPostConfig";
+import { setFetch } from "@app/services/setFetch";
+import { Questions } from "@app/types";
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const useGameCodeForm = () => {
+	const navigate = useNavigate();
+
+	const [nickname, setNickname] = useState("")
+	const [gameCode, setGameCode] = useState<null | number>(null)
+	const [errorMsgs, setErrorMsgs] = useState<string[]>([])
+
+	const { questionsDispatch } = useContext(questionsContext) as Questions;
+	const changeNickname = (evt: React.ChangeEvent<HTMLInputElement>) => setNickname(evt.target.value)
+	const changeGameCode = (evt: React.ChangeEvent<HTMLInputElement>) => setGameCode(Number(evt.target.value))
+	const bothInputsFilled = nickname && gameCode;
+
+	const enterGame = () => {
+		const endpoint = API_ROOT + `/user/${gameCode}/join`
+		const config = getPostConfig({ nickname })
+
+		try {
+			setFetch({
+				endpoint,
+				config,
+				callback: (data) => {
+					const {gameQuestions: questions, gameUsers, gameCode} = data;
+					const payload = {questions, gameUsers, gameCode, nickname}
+
+					questionsDispatch({ type: Q_TYPES.userJoins, payload });
+					navigate(`/game/${gameCode}/room`)
+				}
+			})
+		} catch (err) {
+			setErrorMsgs(prev => [
+				...prev,
+				(err as Error).message
+			]);
+		}
+	}
+
+	return {
+		changeGameCode,
+		changeNickname,
+		errorMsgs,
+		enterGame,
+		bothInputsFilled
+	}
+}
--- src/pages/GameCode/index.tsx
+++ src/pages/GameCode/index.tsx
@@ -1,51 +1,12 @@
-import { useContext, useState } from "react";
 import { Form } from "@app/containers/Form"
 import { Input } from "@app/components/Input"
 import { ButtonsContainer } from "@app/containers/ButtonsContainer"
 import { Button } from "@app/components/Button"
-import { API_ROOT, Q_TYPES } from "@app/globals"
 import { ErrorMsgList } from "@app/containers/ErrorMsgList";
-import { questionsContext } from "@app/contexts/questions.context";
-import { Questions } from "@app/types";
-import { useNavigate } from "react-router-dom";
-import { getPostConfig } from "@app/services/getPostConfig";
-import { setFetch } from "@app/services/setFetch";
+import { useGameCodeForm } from "@app/hooks/useGameCodeForm";
 
 export const GameCode = () => {
-	const navigate = useNavigate();
-
-	const [nickname, setNickname] = useState("")
-	const [gameCode, setGameCode] = useState<null | number>(null)
-	const [errorMsgs, setErrorMsgs] = useState<string[]>([])
-
-	const { questionsDispatch } = useContext(questionsContext) as Questions;
-	const changeNickname = (evt: React.ChangeEvent<HTMLInputElement>) => setNickname(evt.target.value)
-	const changeGameCode = (evt: React.ChangeEvent<HTMLInputElement>) => setGameCode(Number(evt.target.value))
-	const bothInputsFilled = nickname && gameCode;
-
-	const enterGame = () => {
-		const endpoint = API_ROOT + `/user/${gameCode}/join`
-		const config = getPostConfig({ nickname })
-
-		try {
-			setFetch({
-				endpoint,
-				config,
-				callback: (data) => {
-					const {gameQuestions: questions, gameUsers, gameCode} = data;
-					const payload = {questions, gameUsers, gameCode, nickname}
-
-					questionsDispatch({ type: Q_TYPES.userJoins, payload });
-					navigate(`/game/${gameCode}/room`)
-				}
-			})
-		} catch (err) {
-			setErrorMsgs(prev => [
-				...prev,
-				(err as Error).message
-			]);
-		}
-	}
+	const { changeGameCode, changeNickname, errorMsgs, enterGame, bothInputsFilled } = useGameCodeForm()
 
 	return (
 		<section className="page-container">
