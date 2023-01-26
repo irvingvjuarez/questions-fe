533dddab6695af2481a528117a86d83bbb739a1f
Irving Juárez <irvingjuarez274@gmail.com>
2023-01-26 16:38:45 -0600

    Three browsers created per each user using the app in the test

--- tests/create-game.spec.ts
+++ tests/create-game.spec.ts
@@ -1,8 +1,18 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, BrowserType, Browser, Page } from '@playwright/test';
 import { chromium } from "@playwright/test"
 
-let browser, newpage, gameCode
 const characterName = "Vladi"
+let gameCode, adminPage, userPage, user2Page
+
+const browsers: Array<{name: string; browser: Browser | undefined; page: Page | undefined}> = [{
+		name: "admin", browser: undefined, page: undefined
+	},
+	{
+		name: "user", browser: undefined, page: undefined
+	},
+	{
+		name: "user2", browser: undefined, page: undefined
+}]
 
 const questions = [
 	{
@@ -50,76 +60,88 @@ const questions = [
 ]
 
 test.beforeAll(async () => {
-	browser = await chromium.launch()
-	newpage = await browser.newPage()
+	for(let item of browsers) {
+		item.browser = await chromium.launch()
+		item.page = await item.browser.newPage()
+	}
+
+	adminPage = browsers[0].page as Page
+	userPage = browsers[1].page as Page
+	user2Page = browsers[2].page as Page
+})
+
+test.afterAll(() => {
+	for(let item of browsers) {
+		(item.browser as Browser).close()
+	}
 })
 
 test.describe("Making sure the whole game process works fine", () => {
 
-	test("Making sure the game can be created", async ({ page }) => {
-		await page.goto('/');
-		await page.click("a[href='/game/new']")
-		expect(page.url()).toMatch("game/new")
+	test("Making sure the game can be created", async () => {
+		await adminPage.goto('/');
+		await adminPage.click("a[href='/game/new']")
+		expect(adminPage.url()).toMatch("game/new")
 
-		const addOptionBtn = page.getByText("Add another answer option")
+		const addOptionBtn = adminPage.getByText("Add another answer option")
 		expect(addOptionBtn).toBeDisabled()
 
-		await page.waitForSelector("input[name='questionInput']")
+		await adminPage.waitForSelector("input[name='questionInput']")
 
 		for(let questionIndex in questions) {
 			const question = questions[questionIndex]
 
-			await page.type("input[name='questionInput']", question.content)
+			await adminPage.type("input[name='questionInput']", question.content)
 
 			for(let index in question.options) {
 				const option = question.options[index]
 
-				await page.type(option.selector, option.content)
+				await adminPage.type(option.selector, option.content)
 				await addOptionBtn.click()
 			}
 
-			const goBtn = page.getByText("GO!")
+			const goBtn = adminPage.getByText("GO!")
 			await goBtn.click()
 
-			await page.waitForSelector(".page-container > span.highlighted")
-			expect(page.url()).toMatch("/options")
+			await adminPage.waitForSelector(".page-container > span.highlighted")
+			expect(adminPage.url()).toMatch("/options")
 
 			const index = Number(questionIndex)
 
 			if (index === 0) {
-				const anotherQuestionBtn = page.getByText("Add another question")
+				const anotherQuestionBtn = adminPage.getByText("Add another question")
 				expect(anotherQuestionBtn).toBeDisabled()
 
-				await page.click("label[tabindex='1']")
+				await adminPage.click("label[tabindex='1']")
 				await anotherQuestionBtn.click()
 
 			} else if (index === 1) {
-				const createGameBtn = page.getByText("Create Game")
+				const createGameBtn = adminPage.getByText("Create Game")
 				expect(createGameBtn).toBeDisabled()
 
-				await page.click("label[tabindex='4']")
+				await adminPage.click("label[tabindex='4']")
 				await createGameBtn.click()
 
-				await page.waitForTimeout(1000)
-				await page.waitForSelector("span.highlighted")
-				gameCode = await page.$eval("span.highlighted", (el) => el.textContent)
+				await adminPage.waitForTimeout(1000)
+				await adminPage.waitForSelector("span.highlighted")
+				gameCode = await adminPage.$eval("span.highlighted", (el) => el.textContent)
 
-				expect(page.url()).toMatch("/room")
+				expect(adminPage.url()).toMatch("/room")
 			}
 		}
 
 		// Using the second browser
-		await newpage.goto("/game/code")
-		const enterGameBtn = newpage.locator("button")
+		await userPage.goto("/game/code")
+		const enterGameBtn = userPage.locator("button")
 
-		await newpage.type("input[type='number']", gameCode)
-		await newpage.type("input[type='text']", characterName)
+		await userPage.type("input[type='number']", gameCode)
+		await userPage.type("input[type='text']", characterName)
 
 		expect(enterGameBtn).not.toBeDisabled()
 		await enterGameBtn.click()
 
-		await newpage.waitForSelector("h2.subtitle")
-		const character = newpage.getByText(characterName + " (You)")
+		await userPage.waitForSelector("h2.subtitle")
+		const character = userPage.getByText(characterName + " (You)")
 		expect(character).toBeVisible()
 	})
 
