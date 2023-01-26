a640d8322742f0189a5a7987304fa03f8d337a39
Irving Juárez <irvingjuarez274@gmail.com>
2023-01-26 12:58:01 -0600

    Test improved

--- tests/create-game.spec.ts
+++ tests/create-game.spec.ts
@@ -45,51 +45,75 @@ const questions = [
 	}
 ]
 
-test("Making sure the game can be created", async ({ page }) => {
-	await page.goto('/');
-	await page.click("a[href='/game/new']")
-	expect(page.url()).toMatch("game/new")
+test.describe("Making sure the whole game process works fine", () => {
+	let gameCode
 
-	const addOptionBtn = page.getByText("Add another answer option")
-	expect(addOptionBtn).toBeDisabled()
+	test("Making sure the game can be created", async ({ page }) => {
+		await page.goto('/');
+		await page.click("a[href='/game/new']")
+		expect(page.url()).toMatch("game/new")
 
-	await page.waitForSelector("input[name='questionInput']")
+		const addOptionBtn = page.getByText("Add another answer option")
+		expect(addOptionBtn).toBeDisabled()
 
-	for(let questionIndex in questions) {
-		const question = questions[questionIndex]
+		await page.waitForSelector("input[name='questionInput']")
 
-		await page.type("input[name='questionInput']", question.content)
+		for(let questionIndex in questions) {
+			const question = questions[questionIndex]
 
-		for(let index in question.options) {
-			const option = question.options[index]
+			await page.type("input[name='questionInput']", question.content)
 
-			await page.type(option.selector, option.content)
-			await addOptionBtn.click()
+			for(let index in question.options) {
+				const option = question.options[index]
+
+				await page.type(option.selector, option.content)
+				await addOptionBtn.click()
+			}
+
+			const goBtn = page.getByText("GO!")
+			await goBtn.click()
+
+			await page.waitForSelector(".page-container > span.highlighted")
+			expect(page.url()).toMatch("/options")
+
+			const index = Number(questionIndex)
+
+			if (index === 0) {
+				const anotherQuestionBtn = page.getByText("Add another question")
+				expect(anotherQuestionBtn).toBeDisabled()
+
+				await page.click("label[tabindex='1']")
+				await anotherQuestionBtn.click()
+
+			} else if (index === 1) {
+				const createGameBtn = page.getByText("Create Game")
+				expect(createGameBtn).toBeDisabled()
+
+				await page.click("label[tabindex='4']")
+				await createGameBtn.click()
+
+				await page.waitForTimeout(1000)
+				expect(page.url()).toMatch("/room")
+
+				// gameCode = page.$eval("span.highlighted", (el) => el.textContent)
+			}
 		}
 
-		const goBtn = page.getByText("GO!")
-		await goBtn.click()
 
-		await page.waitForSelector(".page-container > span.highlighted")
-		expect(page.url()).toMatch("/options")
+		// test.describe("Making sure the user is able to join the game", () => {
+		// 	test("Entering the correct game code", async ({ page }) => {
+		// 		await page.goto("/game/code")
+		// 		const btn = page.getByText("Enter to the Game!")
 
-		const index = Number(questionIndex)
+		// 		expect(btn).toBeDisabled()
 
-		if (index === 0) {
-			const anotherQuestionBtn = page.getByText("Add another question")
-			expect(anotherQuestionBtn).toBeDisabled()
+		// 		await page.type("input[type='number']", gameCode)
+		// 		await page.type("input[type='text']", "Vladi")
 
-			await page.click("label[tabindex='1']")
-			await anotherQuestionBtn.click()
+		// 		expect(btn).not.toBeDisabled()
+		// 	})
+		// })
 
-		} else if (index === 1) {
-			const createGameBtn = page.getByText("Create Game")
-			expect(createGameBtn).toBeDisabled()
-
-			await page.click("label[tabindex='4']")
-			await createGameBtn.click()
-			expect(page.url()).toMatch("/room")
-		}
-	}
+	})
 
 })
