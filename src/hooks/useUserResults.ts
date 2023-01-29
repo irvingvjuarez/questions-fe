b7531a6491f661442a51b121bf198aa8b23d907f
Irving Juárez <irvingjuarez274@gmail.com>
2023-01-29 11:04:56 -0600

    New hook added for UserResults page

---
+++ src/hooks/useUserResults.ts
@@ -0,0 +1,55 @@
+import { questionsContext } from "@app/contexts/questions.context"
+import { API_ROOT, Q_TYPES } from "@app/globals"
+import { Questions, Score } from "@app/types"
+import { useContext, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import { useErrorValidation } from "./useErrorValidation"
+import { useFetch } from "./useFetch"
+
+export const useUserResults = () => {
+	const setFetch = useFetch()
+	const validation = useErrorValidation()
+	const navigate = useNavigate()
+	let gameRestartedInterval: NodeJS.Timer
+
+	const { answeredQuestion, user: {nickname}, questionsDispatch, gameCode } = useContext(questionsContext) as Questions
+
+	const isUserRight = answeredQuestion?.isUserCorrect
+
+	const userData = answeredQuestion?.answeredBy.find(user => user.userNickname == nickname)
+
+	const isGameRestarted = () => {
+		try {
+			setFetch({
+				endpoint: API_ROOT + `/game/${gameCode}/current/question/full/status`,
+				callback: (data) => {
+					let navigateEndpoint
+
+					if (data.isGameOver) navigateEndpoint = `/game/${gameCode}/over`
+					if (data.status.counterActive) navigateEndpoint = `/game/${gameCode}/current/question`
+
+					if (navigateEndpoint) {
+						questionsDispatch({
+							type: Q_TYPES.clearAnsweredQuestion,
+							payload: { score: data.sortedScore }
+						})
+
+						clearInterval(gameRestartedInterval)
+						navigate(navigateEndpoint)
+					}
+				}
+			})
+		} catch(err) {
+			navigate("/")
+		}
+	}
+
+	useEffect(() => {
+		validation()
+		gameRestartedInterval = setInterval(isGameRestarted, 1000)
+
+		return () => clearInterval(gameRestartedInterval)
+	}, [])
+
+	return { isUserRight, userData }
+}
--- src/pages/UserResults/index.tsx
+++ src/pages/UserResults/index.tsx
@@ -1,58 +1,9 @@
-import { questionsContext } from "@app/contexts/questions.context"
-import { Action, Questions } from "@app/types"
-import { useContext, useEffect } from "react"
-
 import right from "@app/assets/right.png"
 import wrong from "@app/assets/wrong.png"
-import { useErrorValidation } from "@app/hooks/useErrorValidation"
-import { API_ROOT, Q_TYPES } from "@app/globals"
-import { useNavigate } from "react-router-dom"
-import { useFetch } from "@app/hooks/useFetch"
+import { useUserResults } from "@app/hooks/useUserResults"
 
 export const UserResults = () => {
-	const setFetch = useFetch()
-	const validation = useErrorValidation()
-	const navigate = useNavigate()
-	let gameRestartedInterval: NodeJS.Timer
-
-	const { answeredQuestion, user: {nickname}, questionsDispatch, gameCode } = useContext(questionsContext) as Questions
-
-	const isUserRight = answeredQuestion?.isUserCorrect
-
-	const userData = answeredQuestion?.answeredBy.find(user => user.userNickname == nickname)
-
-	const isGameRestarted = () => {
-		try {
-			setFetch({
-				endpoint: API_ROOT + `/game/${gameCode}/current/question/full/status`,
-				callback: (data) => {
-					let navigateEndpoint
-
-					if (data.isGameOver) navigateEndpoint = `/game/${gameCode}/over`
-					if (data.status.counterActive) navigateEndpoint = `/game/${gameCode}/current/question`
-
-					if (navigateEndpoint) {
-						questionsDispatch({
-							type: Q_TYPES.clearAnsweredQuestion,
-							payload: { score: data.sortedScore }
-						})
-
-						clearInterval(gameRestartedInterval)
-						navigate(navigateEndpoint)
-					}
-				}
-			})
-		} catch(err) {
-			navigate("/")
-		}
-	}
-
-	useEffect(() => {
-		validation()
-		gameRestartedInterval = setInterval(isGameRestarted, 1000)
-
-		return () => clearInterval(gameRestartedInterval)
-	}, [])
+	const { isUserRight, userData } = useUserResults()
 
 	return(
 		<section className="page-container h-[70vh] flex flex-col justify-between">
@@ -70,6 +21,7 @@ export const UserResults = () => {
 
 			<p>
 				Current points:
+
 				<span className="highlighted">
 					{userData?.userScore || 0}
 				</span>
