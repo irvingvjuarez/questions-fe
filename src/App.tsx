27806ea6c1869e484da8f07270a3c273335f8603
Irving Juárez <irvingjuarez274@gmail.com>
2023-02-02 10:10:52 -0600

    Helmet added to change title tag

--- src/App.tsx
+++ src/App.tsx
@@ -15,34 +15,37 @@ import { UserScore } from './pages/UserScore'
 import { UserResults } from './pages/UserResults'
 import { GameOver } from './pages/GameOver'
 import { getInitialState } from './services/getInitialState'
+import { HelmetProvider } from 'react-helmet-async'
 
 function App() {
 	const [questions, questionsDispatch] = useReducer(questionsReducer, getInitialState());
 	const questionsValue = { ...questions, questionsDispatch }
 
   return (
-		<BrowserRouter>
-			<Layout>
-					<QuestionsProvider value={questionsValue}>
-						<Routes>
-							<Route index element={<Home />} />
-							<Route path='/game/new' element={<NewGame />} />
-							<Route path='/game/code' element={<GameCode />} />
-							<Route path='/questions/:questionId/options' element={<Options />} />
-							<Route path='/game/questions/new' element={<NewGame />} />
-							<Route path='/game/:gameCode/room' element={<GameRoom />} />
-							<Route path='/game/:gameCode/current/question' element={<CurrentQuestion />}/>
-							<Route path='/game/:gameCode/current/score' element={<CurrentScore />}/>
-							<Route
-								path='/game/:gameCode/user/:nickname/current/score'
-								element={<UserScore />}
-							/>
-							<Route path='/game/:gameCode/user/current/results' element={<UserResults />} />
-							<Route path='/game/:gameCode/over' element={<GameOver />}/>
-						</Routes>
-					</QuestionsProvider>
-			</Layout>
-		</BrowserRouter>
+		<HelmetProvider>
+			<BrowserRouter>
+				<Layout>
+						<QuestionsProvider value={questionsValue}>
+							<Routes>
+								<Route index element={<Home />} />
+								<Route path='/game/new' element={<NewGame />} />
+								<Route path='/game/code' element={<GameCode />} />
+								<Route path='/questions/:questionId/options' element={<Options />} />
+								<Route path='/game/questions/new' element={<NewGame />} />
+								<Route path='/game/:gameCode/room' element={<GameRoom />} />
+								<Route path='/game/:gameCode/current/question' element={<CurrentQuestion />}/>
+								<Route path='/game/:gameCode/current/score' element={<CurrentScore />}/>
+								<Route
+									path='/game/:gameCode/user/:nickname/current/score'
+									element={<UserScore />}
+								/>
+								<Route path='/game/:gameCode/user/current/results' element={<UserResults />} />
+								<Route path='/game/:gameCode/over' element={<GameOver />}/>
+							</Routes>
+						</QuestionsProvider>
+				</Layout>
+			</BrowserRouter>
+		</HelmetProvider>
   )
 }
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,28 +4,33 @@ import { useContext, useEffect } from "react"
 import { questionsContext } from "@app/contexts/questions.context"
 import { Questions } from "@app/types"
 import { Q_TYPES } from "@app/globals"
+import { Helmet } from "react-helmet-async"
 
 export const Home = () => {
 	const { questionsDispatch } = useContext(questionsContext) as Questions;
 	useEffect(() => questionsDispatch({ type: Q_TYPES.clearQuestions }), [])
 
 	return (
-		<section className="page-container h-[76vh] flex flex-col justify-between">
-			<h3 className="subtitle">
-				Create questionnaries easily, quickly and for free.
-			</h3>
+		<>
+			<Helmet>
+				<title>Home | Questions</title>
+			</Helmet>
 
-			<ButtonsContainer>
-				<Button linkUrl="/game/new" variant="standard">
-					Create New Game Room
-				</Button>
+			<section className="page-container h-[76vh] flex flex-col justify-between">
+				<h3 className="subtitle">
+					Create questionnaries easily, quickly and for free.
+				</h3>
 
-				<Button linkUrl="/game/code" variant="standard">
-					Enter Game Code
-				</Button>
-			</ButtonsContainer>
-		</section>
-		// <main className="h-[76vh] flex flex-col justify-between">
-		// </main>
+				<ButtonsContainer>
+					<Button linkUrl="/game/new" variant="standard">
+						Create New Game Room
+					</Button>
+
+					<Button linkUrl="/game/code" variant="standard">
+						Enter Game Code
+					</Button>
+				</ButtonsContainer>
+			</section>
+		</>
 	)
 }
